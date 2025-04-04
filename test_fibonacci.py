becfbdef8ce91d98d36ee55536ab9c5c22241396
Gurinder Singh <92890724+1stgoal@users.noreply.github.com>
2025-04-03 20:57:46 -0400

    3) Test for input of two.

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -23,3 +23,10 @@ def test_input_zero():
 def test_input_one():
     fib_list = [num for num in Fibonacci(1)]
     assert fib_list == [0, 1]
+
+    """Tests for the value two entered"""
+
+
+def test_input_two():
+    fib_list = [num for num in Fibonacci(2)]
+    assert fib_list == [0, 1, 1]
